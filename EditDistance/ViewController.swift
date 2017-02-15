1d7af65775ef25121c2cd32bd7a1ad04a992ad64
kahayash <kahayash@mbp15ual-nc0043.local>
2017-02-15 11:29:42 +0900

    Initial Commit

---
+++ EditDistance/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EditDistance
+//
+//  Created by kahayash on 2017/02/15.
+//  Copyright © 2017年 Kazuhiro Hayashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
